57fc02bc842df76de023b4bc8878879e2b45ff55
Seerat Shaikh <129963092+Seerat-Shaikh@users.noreply.github.com>
2023-06-24 21:44:08 +0500

    cookies primer

--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose'); //For creating new user model
 const { isEmail } = require('validator'); //importing isEmail package from validator we installed
+const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
     email: {
@@ -15,6 +16,21 @@ const userSchema = new mongoose.Schema({
         minlength: [6, 'Minimum password length is 6 character']
     },
 });
+
+// fire a function after doc saved to db
+userSchema.post('save', function (doc, next) { //post is referring to something happening after firing a function to database
+    console.log('new user was created & saved', doc);
+    next();
+});
+
+// fire a function before doc saved to db
+userSchema.pre('save', async function (next) { //pre is before
+    //console.log('user about to be created & saved', this); //"this" will create local instant of user before we save it to the database
+    const salt = await bcrypt.genSalt();
+    this.password = await bcrypt.hash(this.password, salt); //this refers to instant of user we trying to create
+    next();
+});
+  
 
 //Model based on Schema
 const User = mongoose.model('user', userSchema); //(name, schema) name must be singular what we define for database
