ac12d518c134511ff337ec89d5d67d68bc2fe0e7
Seerat Shaikh <129963092+Seerat-Shaikh@users.noreply.github.com>
2023-06-26 21:40:03 +0500

    Conditional Rendering

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 const authRoutes = require('./routes/authRoutes');
 const cookieParser = require('cookie-parser');
+const { requireAuth, checkUser } = require('./middleware/authMiddleware');
+
 
 const app = express();
 
@@ -22,28 +24,7 @@ mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCr
   .catch((err) => console.log(err));
 
 // routes
+app.get('*', checkUser); // * use to apply in all routes
 app.get('/', (req, res) => res.render('home'));
-app.get('/smoothies', (req, res) => res.render('smoothies'));
+app.get('/smoothies', requireAuth, (req, res) => res.render('smoothies'));
 app.use(authRoutes);
-
-// cookies
-
-app.get('/set-cookies', (req, res) => {
-
-  // res.setHeader('Set-Cookie', 'newUser=true');
-  
-  res.cookie('newUser', false);
-  res.cookie('isEmployee', true, { maxAge: 1000 * 60 * 60 * 24, httpOnly: true }); //httponly & secure can use only when secure authentications u wnat to make
-
-  res.send('you got the cookies!');
-
-});
-
-app.get('/read-cookies', (req, res) => {
-
-  const cookies = req.cookies;
-  console.log(cookies.newUser);
-
-  res.json(cookies);
-
-});
